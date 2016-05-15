eeb24809c776db41fd363559e32f4260a84aa8c2
nfactorial <nfactorial@btopenworld.com>
2016-05-15 10:57:31 +0100

    Updated tank controller script.

--- development/scripts/entities/tank/tank_controller.js
+++ development/scripts/entities/tank/tank_controller.js
@@ -12,6 +12,16 @@ pc.script.attribute('maxHealth', 'number', 100, {
     description: 'The maximum health value for this unit.'
 });
 
+pc.script.attribute('turret', 'entity', null, {
+    displayName: 'Turret',
+    description: 'The turret that belongs to our tank entity.'
+});
+
+pc.script.attribute('base', 'entity', null, {
+    displayName: 'Base',
+    description: 'The base of our tank entity.'
+});
+
 pc.script.create('tank_controller', function (app) {
     var TankController = function (entity) {
         this.entity = entity;
@@ -34,6 +44,43 @@ pc.script.create('tank_controller', function (app) {
          * @param dt {Number} The time (in seconds) sicne the last update.
          */
         update: function(dt) {
+            this.updateAttack(dt);
+            this.updateMovement(dt);
+        },
+
+        /**
+         * Performs any per-frame processing associated with our attack target.
+         * @param dt {Number} Time (in seconds) since the last update.
+         */
+        updateAttack: function(dt) {
+            // If we have an attack target, rotate turret to point at it
+            // If we are pointing at our target, fire
+            // If we have no attack target, rotate turret to point at movement target
+        },
+
+        /**
+         * Performs any per-frame processing associated with our movement.
+         * @param dt {Number} Time (in seconds) since the last update.
+         */
+        updateMovement: function(dt) {
+            // Rotate base to point at movement target
+            // Move along direction vector
+        },
+
+        /**
+         * Finds the enemy tower that is closest to our entity.
+         */
+        findMoveTarget: function() {
+            if (this.opponent) {
+                var self = this;
+                var nearest = null;
+                this.opponent.towers.forEach(function(e) {
+                    // Compute distance to tower
+                    // If it's closer than 'nearest' set it as nearest
+                });
+
+                // Set selected tower as our movement target.
+            }
         },
 
         /**
