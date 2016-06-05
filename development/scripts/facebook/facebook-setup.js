f9b9f5dacdc3892d908bc308621cfc4c40e1203e
nfactorial <nfactorial@btopenworld.com>
2016-06-05 11:26:40 +0100

    Added facebook-setup.js to development folder.

---
+++ development/scripts/facebook/facebook-setup.js
@@ -0,0 +1,35 @@
+(function () {
+    var _fb = false;
+    var _pc = false;
+    var _inst = null;
+    var app = null;
+
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId      : '666582873493792',
+            xfbml      : true,
+            version    : 'v2.5'
+        });
+
+        _fb = true;
+
+        if (_fb && app) {
+            app.fire("fb:init");
+        }
+    };
+
+    app = pc.Application.getApplication();
+    app.on("initialize", function () {
+        if (_fb) {
+            app.fire("fb:init");
+        }
+    });
+
+    (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+}());
